92a4d3efdb76a1935e97649e40efff0e2985ce0f
kermekx <kermekx@gmail.com>
2015-04-10 00:14:22 +0200

    Char

---
+++ Virtual War/src/com/otgenasis/virtualwar/robot/Char.java
@@ -0,0 +1,58 @@
+package com.otgenasis.virtualwar.robot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.otgenasis.virtualwar.coordonnees.Coordonnees;
+import com.otgenasis.virtualwar.vue.Vue;
+
+public class Char extends Robot {
+	
+	public List<Coordonnees> deplacements;
+	
+	public Char(Vue vue, int x, int y, int equipe) {
+		super(vue, x, y, equipe);
+		setEnergie(60);
+	}
+
+	@Override
+	public boolean peutTirer() {
+		return true;
+	}
+
+	@Override
+	public int getCoutAction() {
+		return 6;
+	}
+
+	@Override
+	public int getCoutDep() {
+		return 5;
+	}
+
+	@Override
+	public int getCoutTir() {
+		return 1;
+	}
+
+	@Override
+	public int getCoutMine() {
+		return 0;
+	}
+
+	@Override
+	public String getType() {
+		return "Char";
+	}
+
+	@Override
+	public List<Coordonnees> getDeplacements() {
+		deplacements = new ArrayList<Coordonnees>();
+		for(int i = -2; i <= 2; i++)
+			for(int j = -2; j <= 2; j++)
+				if(i == j && !(i == 0))
+					deplacements.add(getCoord().ajout(new Coordonnees(i, j)));
+		return deplacements;
+	}
+
+}
