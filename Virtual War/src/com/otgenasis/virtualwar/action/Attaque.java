0bff40ad6d37244b62046544f2b3ce5107121d51
Alex <roal6262@gmail.com>
2015-04-10 00:28:50 +0200

    Attaque Deplacement

--- Virtual War/src/com/otgenasis/virtualwar/action/Attaque.java
+++ Virtual War/src/com/otgenasis/virtualwar/action/Attaque.java
@@ -1,11 +1,30 @@
 package com.otgenasis.virtualwar.action;
 
+import com.otgenasis.virtualwar.coordonnees.Coordonnees;
+import com.otgenasis.virtualwar.robot.Char;
+import com.otgenasis.virtualwar.robot.Piegeur;
+import com.otgenasis.virtualwar.robot.Robot;
+import com.otgenasis.virtualwar.robot.Tireur;
+
 public class Attaque extends Action {
+
+	public Attaque(Robot robot, Coordonnees c) {
+		super(robot, c);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	void agit() {
-		// TODO Auto-generated method stub
-		
+		if(this.robot instanceof Tireur) {
+			this.robot.setEnergie(this.robot.getEnergie()-2);
+			
+		} else if (this.robot instanceof Piegeur) {
+			this.robot.setEnergie(this.robot.getEnergie()-2);
+			
+		} else if (this.robot instanceof Char) {
+			this.robot.setEnergie(this.robot.getEnergie()-1);
+			
+		}
 	}
 
 }
--- Virtual War/src/com/otgenasis/virtualwar/action/Deplacement.java
+++ Virtual War/src/com/otgenasis/virtualwar/action/Deplacement.java
@@ -1,11 +1,18 @@
 package com.otgenasis.virtualwar.action;
 
+import com.otgenasis.virtualwar.coordonnees.Coordonnees;
+import com.otgenasis.virtualwar.robot.Robot;
+import com.otgenasis.virtualwar.robot.Tireur;
+
 public class Deplacement extends Action {
 	
+	public Deplacement(Robot robot, Coordonnees c) {
+		super(robot, c);
+	}
+
 	@Override
 	void agit() {
-		// TODO Auto-generated method stub
-		
+		this.robot.setCoord(this.getDirection());
 	}
 
 }
